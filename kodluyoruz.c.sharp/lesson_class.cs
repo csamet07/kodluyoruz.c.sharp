4dc6d15ae99eb343a592285b7b3d876e028c53b4
ARGE2 <arge2@tekviya.com>
2021-05-10 18:07:49 +0300

    lesson class added

---
+++ kodluyoruz.c.sharp/lesson_class.cs
@@ -0,0 +1,23 @@
+﻿namespace kodluyoruz.c.sharp
+{
+    public class lesson_class
+    {
+        public void example()
+        {
+            var worker1 = new worker();
+
+            worker1.name = "samet";
+            worker1.departman = "yazılım";
+            worker1.number = 123;
+            worker1.surname = "can";
+        }
+    }
+
+    public class worker
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int number { get; set; }
+        public string departman { get; set; }
+    }
+}
