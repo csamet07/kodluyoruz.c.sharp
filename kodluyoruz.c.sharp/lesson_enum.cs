fd2aa76171e187c1c36342c33d9e89d07e3fb201
ARGE2 <arge2@tekviya.com>
2021-05-11 15:20:46 +0300

    lesson enum added

---
+++ kodluyoruz.c.sharp/lesson_enum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace kodluyoruz.c.sharp
+{
+    public class lesson_enum
+    {
+        public void example()
+        {
+            Console.WriteLine(gunler.çarşamba);
+            Console.WriteLine((int)gunler.cuma);
+
+            int sicaklık = 32;
+
+            if (sicaklık <= (int)hava_durumu.normal)
+            {
+                Console.WriteLine("ısınmasını bekle");
+            } else if (sicaklık <= (int)hava_durumu.sıcak)
+            {
+                Console.WriteLine("hava sıcak");
+            }else if (sicaklık <= (int)hava_durumu.çok_sıcak)
+            {
+                Console.WriteLine("çıkabilirsin");
+            }
+        }
+    }
+
+    enum gunler
+    {
+        pazartesi = 1,
+        salı,
+        çarşamba,
+        perşembe,
+        cuma,
+        cumartesi,
+        pazar
+    }
+
+    enum hava_durumu
+    {
+        soguk = 5,
+        normal = 20,
+        sıcak = 25,
+        çok_sıcak = 30
+    }
+}
