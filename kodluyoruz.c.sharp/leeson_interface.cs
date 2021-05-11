d8ba95963636d25b5b58122187c81e5a7f0b746b
ARGE2 <arge2@tekviya.com>
2021-05-11 17:11:07 +0300

    lesson interface added

---
+++ kodluyoruz.c.sharp/leeson_interface.cs
@@ -0,0 +1,45 @@
+﻿namespace kodluyoruz.c.sharp
+{
+    public class leeson_interface
+    {
+        
+    }
+
+    interface IOtomobil
+    {
+        int kac_tekerlegi_var();
+        marka hangi_markanin_araci();
+        renk standart_rengi_ne();
+    }
+
+    enum marka
+    {
+        ford,
+        toyota,
+        honda
+    }
+
+    enum renk
+    {
+        beyaz,
+        gri
+    }
+
+    class focus : IOtomobil
+    {
+        public int kac_tekerlegi_var()
+        {
+            return 4;
+        }
+
+        public marka hangi_markanin_araci()
+        {
+            return marka.ford;
+        }
+
+        public renk standart_rengi_ne()
+        {
+            return renk.beyaz;
+        }
+    }
+}
